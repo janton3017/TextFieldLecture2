154f5dcb9240b1d2ddb06136785301ca809496cf
Justin <janton3017@stu.d214.org>
2020-11-09 22:21:43 -0800

    Added code to add the textfields together.

--- TextFieldLecture2/ViewController.swift
+++ TextFieldLecture2/ViewController.swift
@@ -25,7 +25,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonWasPressed(_ sender: Any) {
+        let number1string = textField1.text!
+        let number2string = textField2.text!
         
+        let number1 = Int(number1string)!
+        let number2 = Int(number2string)!
+        
+        
+        let answer = number1 + number2
+        
+        outputLabel.text = "\(answer)"
     }
     
 }
